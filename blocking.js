f4690344d54637fc01f28301eb37b540fb39f685
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-07-09 22:16:12 +1200

    creating a new project folder and updating blocking.js code

--- blocking.js
+++ blocking.js
@@ -1,7 +1,10 @@
 var getUserSync = require('./getUserSync');
+
+console.log('Starting user1');
 var user1 =getUserSync('123');
 console.log('user1',user1);
 
+console.log('Starting user2')
 var user2 = getUserSync('321');
 console.log('user2',user2);
 
