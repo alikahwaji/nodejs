7bad72c4b8cf3f060fb64e77257d433c257ce27e
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-07-09 22:42:44 +1200

    updating the code

--- Notes-node/app.js
+++ Notes-node/app.js
@@ -1,1 +1,7 @@
-creating an app js file for notes-node 
+//creating an app js file for notes-node 
+
+console.log('Starting app.');
+
+const fs = require('fs');
+
+fs.appendFile('greetings.txt', 'Hello World!');
