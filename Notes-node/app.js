7e2c969487e5e4e9a3966998c4d8b882b45134ef
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-07-10 20:13:59 +1200

    Creating module.exports with node js for note app

--- Notes-node/app.js
+++ Notes-node/app.js
@@ -1,11 +1,17 @@
 //creating an app js file for notes-node 
 
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os =require ('os');
+const notes = require ('./notes.js')
 
-var user = os.userInfo();
+// var res =notes.addNote();
+// console.log(res);
 
-fs.appendFile('greetings.txt', `Hello ${user.username} !`);
+// var user = os.userInfo();
 
+// fs.appendFile('greetings.txt', `Hello ${user.username}. You are ${notes.age}.`);
+
+var sum = notes.addNumber(9, -2);
+console.log('addNumber= '+ sum);
---
+++ Notes-node/notes.js
@@ -0,0 +1,16 @@
+console.log('Strating notes.js');
+
+// console.log(module);
+
+// module.exports.age = 25;
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New Note';
+
+}
+
+module.exports.addNumber =  (a,b) => {
+    // console.log('addNumber');
+    return a+b;
+}
