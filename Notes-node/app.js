6bfda79cf33849fef1181b9f5b27e36d83fe16c4
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-07-10 20:43:50 +1200

    using 3rd party modules

--- Notes-node/app.js
+++ Notes-node/app.js
@@ -4,7 +4,11 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os =require ('os');
-const notes = require ('./notes.js')
+const notes = require ('./notes.js');
+const _ = require('lodash');
+
+// console.log(_.isString(true));
+// console.log(_.isString('Ali'));
 
 // var res =notes.addNote();
 // console.log(res);
@@ -13,5 +17,8 @@ const notes = require ('./notes.js')
 
 // fs.appendFile('greetings.txt', `Hello ${user.username}. You are ${notes.age}.`);
 
-var sum = notes.addNumber(9, -2);
-console.log('addNumber= '+ sum);
+// var sum = notes.addNumber(9, -2);
+// console.log('addNumber= '+ sum);
+
+var filteredArray = _.uniq(['Ali', 2, 'Ali', 1,2,3,4]);
+console.log(filteredArray);
