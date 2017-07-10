61d060ee78fef40dcaa9f68e49789353af5a7c3f
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-07-10 23:06:27 +1200

    parse command arugs

--- Notes-node/app.js
+++ Notes-node/app.js
@@ -1,6 +1,6 @@
 //creating an app js file for notes-node 
 
-console.log('Starting app.js');
+//console.log('Starting app.js');
 
 // const os =require ('os');
 // console.log(_.isString(true));
@@ -20,3 +20,27 @@ console.log('Strating app.js');
 const fs = require('fs');
 const notes = require ('./notes.js');
 const _ = require('lodash');
+const yargs = require ('yargs');
+
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+// console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+if (command === 'add'){
+    //console.log('Adding new note');
+    notes.addNote(argv.title, argv.body)
+} else if (command === 'list') {
+    //console.log('Listing all notes');
+    notes.getAll();
+} else if (command === 'read') { 
+    //console.log('Reading notes');
+    notes.getReadAll(argv.title);
+} else if (command === 'remove') {
+    //console.log('Removing notes')
+    notes.getRemoveNote(argv.title)
+}  else {
+    console.log('Command not recongized');
+}
--- Notes-node/notes.js
+++ Notes-node/notes.js
@@ -4,14 +4,34 @@
 //     // console.log('addNumber');
 //     return a+b;
 // }
+// module.exports.addNote = () => {
+//     console.log('addNote');
+//     return 'New Note';
+// }
+
 /*----------------------------------------------*/
 
 console.log('Strating notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New Note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
+};
 
-}
+var getAll = () => {
+    console.log('Getting all notes');
+};
 
+var getReadAll = (title) => {
+    console.log('Reading note ',title);
+};
 
+var getRemoveNote = (title) => {
+    console.log('Removing note ',title);
+};
+
+module.exports ={
+    addNote: addNote,
+    getAll: getAll,
+    getReadAll: getReadAll,
+    getRemoveNote: getRemoveNote
+};
