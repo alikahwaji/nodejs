ca587aaebd247e91184353beae3229340a000283
alikahwaji <alikahwaji.kahwaji@hotmail.com>
2017-07-31 17:22:06 +1200

    Updating files

--- Notes-node/app.js
+++ Notes-node/app.js
@@ -31,7 +31,7 @@ console.log('Yargs', argv);
 
 if (command === 'add'){
     //console.log('Adding new note');
-    notes.addNote(argv.title, argv.body)
+    var note = notes.addNote(argv.title, argv.body)
 } else if (command === 'list') {
     //console.log('Listing all notes');
     notes.getAll();
