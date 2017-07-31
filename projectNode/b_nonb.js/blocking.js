7edf2f7aa8cdd3e0141e9d3e95380fe8b89793cd
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-07-31 21:45:21 +1200

    Finished I/O files

--- projectNode/b_nonb.js/blocking.js
+++ projectNode/b_nonb.js/blocking.js
@@ -1,8 +1,10 @@
 var getUserSync = require('./getUserSync')
 
+console.log('starting user1')
 var user1 = getUserSync('123')
 console.log('user1', user1)
 
+console.log('starting user2')
 var user2 = getUserSync('321')
 console.log('user2', user2)
 
--- projectNode/b_nonb.js/non_blocking.js
+++ projectNode/b_nonb.js/non_blocking.js
@@ -1,9 +1,11 @@
 var getUserSync = require('./getUserSync')
 
+console.log('starting user1')
 getUser('123', function(user1) {
     console.log('user1', user1)
 })
 
+console.log('starting user2')
 getUser('321', function(user2) {
     console.log('user2', user2)
 })
