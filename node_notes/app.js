5cee2ab76144e105e879b91fee41d927f1c1db0e
alikahwaji <ali.kahwaji@hotmail.com>
2017-08-23 13:15:07 +1200

    requiring our own modules

--- node_notes/app.js
+++ node_notes/app.js
@@ -2,11 +2,27 @@ console.log('Starting App')
 
 const fs = require('fs')
 const os = require('os')
+const notes = require('./notes')
 
-var user = os.userInfo()
+var res = notes.addNote()
+console.log(res)
 
-fs.appendFile('greeting.txt', ` Hello ${user.username}`)
+var result = notes.addNumbers(5, 4)
+console.log(result)
 
-fs.appendFile('goodbye.txt', 'See you soon')
+var resultSub = notes.subNumbers(10, 8)
+console.log(resultSub)
 
-fs.appendFile('info-card.txt', 'Ali Ghalib Younis Kahwaji')
+var resultDiv = notes.divNumbers(12, 3)
+console.log(resultDiv)
+
+var resultMulti = notes.multiNumbers(20, 4)
+console.log(resultMulti)
+
+// var user = os.userInfo()
+
+// fs.appendFile('greeting.txt', ` Hello ${user.username} ! You are ${notes.age}. `)
+
+// fs.appendFile('goodbye.txt', 'See you soon')
+
+// fs.appendFile('info-card.txt', 'Ali Ghalib Younis Kahwaji')
