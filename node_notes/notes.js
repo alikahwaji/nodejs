91079462c8b8cb7e085d9a571b4956dd57ef88dc
alikahwaji <ali.kahwaji@hotmail.com>
2017-08-23 13:49:37 +1200

    arranging files and creating an example.js

--- node_notes/notes.js
+++ node_notes/notes.js
@@ -1,28 +1,6 @@
 console.log('Starting Notes')
 
-// console.log(module)
-
 module.exports.addNote = () => {
   console.log('Added Note')
   return 'New Note'
-}
-
-module.exports.addNumbers = (a, b) => {
-  console.log('Adding two numbers')
-  return a + b
-}
-
-module.exports.subNumbers = (a, b) => {
-  console.log('Sub numbers')
-  return a - b
-}
-
-module.exports.divNumbers = (a, b) => {
-  console.log('Div numbers')
-  return a / b
-}
-
-module.exports.multiNumbers = (a, b) => {
-  console.log('Multi numbers')
-  return a * b
 }
