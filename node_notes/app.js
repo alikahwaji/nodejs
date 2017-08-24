0f75630b953182fa45cdf328c672401cd6cdee70
alikahwaji <ali.kahwaji@hotmail.com>
2017-08-24 14:44:27 +1200

    using argv for using command line without parsering

--- node_notes/app.js
+++ node_notes/app.js
@@ -4,3 +4,21 @@ const _ = require('lodash')
 const fs = require('fs')
 
 const notes = require('./notes')
+
+var command = process.argv[2]
+console.log('Command : ', command)
+console.log(process.argv)
+
+if (command === 'add') {
+  console.log('Adding new note')
+} else if (command === 'list') {
+  console.log('listing all the note')
+} else if (command === 'read') {
+  console.log('Fetaching a note')
+} else if (command === 'remove') {
+  console.log('Removing a note')
+} else if (command === 'edit') {
+  console.log('Editing a note')
+} else {
+  console.log('Command not found')
+}
