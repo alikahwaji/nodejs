dc1123f355c42a44c9d29a1be757a473faf3625a
alikahwaji <ali.kahwaji@hotmail.com>
2017-08-26 15:32:38 +1200

    getting weather data from forecast website

--- node_weather/app.js
+++ node_weather/app.js
@@ -1,23 +1,38 @@
 
-const yargs = require('yargs')
-const geocode = require('./geocode/geocode')
+// const yargs = require('yargs')
+// const geocode = require('./geocode/geocode')
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address for weather report',
-      string: true
-    }})
-  .help()
-  .alias('help', 'h')
-  .argv
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address for weather report',
+//       string: true
+//     }})
+//   .help()
+//   .alias('help', 'h')
+//   .argv
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage)
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage)
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2))
+//   }
+// })
+
+const request = require('request')
+
+request({
+  url: 'https://api.darksky.net/forecast/ac82990c1895d75cec402eaa7dc9e9fd/37.7888223,-83.41997909999999',
+  json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to forecast.io')
+  } else if (!error && response.statusCode === 200) {
+    console.log(`Current temperature : ${body.currently.temperature}`)
   } else {
-    console.log(JSON.stringify(results, undefined, 2))
+    console.log('Unable to get weather data')
   }
 })
