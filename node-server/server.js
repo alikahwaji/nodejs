a17220ac82ce6a6571755d7f428d13bbee0a1289
alikahwaji <ali.kahwaji@hotmail.com>
2017-08-27 12:19:04 +1200

    setting up server routes

---
+++ node-server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+var app = express()
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello and woelcome to the weather app</h1>')
+  res.send({
+    name: 'Ali',
+    likes: [
+      'Barcelona',
+      'Messi'
+    ]
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.send('About us')
+})
+
+app.get('/error', (req, res) => {
+  res.send({
+    errorMessage: 'Link broken'
+  })
+})
+
+app.listen(3000)
