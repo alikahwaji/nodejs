12ec68dde4e517649364a1ae23b5d7d6e1f5986b
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-08 09:21:42 +1200

    Cresating 2nd test and check for passing

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -7,3 +7,11 @@ it('should add two numbers', () => {
     throw new Error(`Expected 50, but got ${res}.`)
   }
 })
+
+it('Should square a number', () => {
+  var res = utils.square(10)
+
+  if (res !== 100) {
+    throw new Error(`Expected 100, but got ${res}.`)
+  }
+})
