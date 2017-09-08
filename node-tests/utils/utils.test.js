20c74eca2f534dbcb7f2bef06d5323828bbf8e5d
alikahwaji <ali.kahwaji@hotmail.com>
2017-09-09 09:38:00 +1200

    installing expect assertion for refactoring our test code

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,31 +1,24 @@
 const utils = require('./utils')
+const expect = require('expect')
 
 it('should add two numbers', () => {
   var res = utils.add(30, 20)
 
-  if (res !== 50) {
-    throw new Error(`Expected 50, but got ${res}.`)
-  }
+  expect(res).toBe(50).toBeA('number')
 })
 
 it('Should square a number', () => {
   var res = utils.square(10)
 
-  if (res !== 100) {
-    throw new Error(`Expected 100, but got ${res}.`)
-  }
+  expect(res).toBe(100).toBeA('number')
 })
 
 it('Should sub two numbers', () => {
   var res = utils.sub(5, 4)
-  if( res !== 1) {
-    throw new Error(`Expected 1, but got ${res}.`)
-  }
+  expect(res).toBe(1).toBeA('number')
 })
 
 it('Should div two numbers', () => {
   var res = utils.div(30, 10)
-  if(res !== 3) {
-    throw new Error(`Expected 3, but got ${res}.`)
-  }
+  expect(res).toBe(3).toBeA('number')
 })
